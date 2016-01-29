b73c33a915756507d1ebe95d21ae6fa707ebd125
chargao <charlie.gao@utexas.edu>
2016-01-29 13:28:30 -0600

    adding starter code little by little

--- Lab1/assembler.c
+++ Lab1/assembler.c
@@ -5,3 +5,25 @@
 	UTEID 2: tc22364 
 */
 
+#include <stdio.h>    /* standard input/output library */
+#include <stdlib.h>    /* Standard C Library */
+#include <string.h>    /* String operations library */
+#include <ctype.h>    /* Library for useful character operations */
+#include <limits.h>    /* Library for definitions of common variable type characteristics */
+
+int
+ main(int argc, char* argv[]) {
+
+     char *prgName   = NULL;
+     char *iFileName = NULL;
+     char *oFileName = NULL;
+
+     prgName   = argv[0];
+     iFileName = argv[1];
+     oFileName = argv[2];
+
+     printf("program name = '%s'\n", prgName);
+     printf("input file name = '%s'\n", iFileName);
+     printf("output file name = '%s'\n", oFileName);
+}
+
