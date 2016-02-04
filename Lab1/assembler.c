9fa3884c46ce33dcfa5c50d5a6791753223ec7de
chargao <charlie.gao@utexas.edu>
2016-02-03 18:14:23 -0600

    -m

--- Lab1/assembler.c
+++ Lab1/assembler.c
@@ -10,15 +10,10 @@
 #include <string.h>    /* String operations library */
 #include <ctype.h>    /* Library for useful character operations */
 #include <limits.h>    /* Library for definitions of common variable type characteristics */
-
-typedef struct{
-    int loc; /*address*/
-    char* label;
-}SymbolEntry;
-
+#define numOpCodes 31
+#define MAX_LABEL_LEN 20
+#define MAX_SYMBOLS 255
 #define MAX_LINE_LENGTH 255
-#define MAX_SYMBOL_COUNT 255
-#define numOpCodes 28
 
 /*Array of valid opcodes*/
 char* codes[numOpCodes] = { "add",
@@ -48,40 +43,40 @@ char* codes[numOpCodes] = { "add",
                             "stb",
                             "stw",
                             "trap",
-                            "xor"
+                            "xor",
+                            ".orig",
+                            ".end",
+                            ".fill"
 };
 
-int* cond;
-
-char* sym_table_labels[500]; /*Names of labels*/
-int sym_table_values[500]; /*Value is offset from PC*/
+typedef struct {
+    int address;
+    char label[MAX_LABEL_LEN + 1];  /* Question for the reader: Why do we need to add 1? */
+} TableEntry;
+TableEntry symbolTable[MAX_SYMBOLS];
 int sym_table_size = 0; /*Current size of symbol table*/
-
-int pc_offset = 0;
+int baseline; /*.orig baseline*/
+int* cond;
 
 enum{
     DONE, OK, EMPTY_LINE
 };
 
 /*Adds label and value to symbol table*/
-void addSymbolTable(char* label, int value)
-{
-    sym_table_labels[sym_table_size] = label;
-    sym_table_values[sym_table_size] = value;
-
+void addSymbolTable(char* label, int value){
+    symbolTable[sym_table_size].address=value;
+    strcpy(symbolTable[sym_table_size].label,label);
     sym_table_size += 1;
 }
 
 /*Returns offset from start of program if label is found*/
-int searchSymbolTable(char* label)
-{
+int searchSymbolTable(char* label){
     int i;
-    for (i = 0; i < sym_table_size; i++)
-    {
-    if (strcmp(sym_table_labels[i], label) == 0) /*Label has been found*/
-        return sym_table_values[i];
+    for (i = 0; i < sym_table_size; i++){
+        printf("comparing intable %s to %s\n", symbolTable[i].label,label);
+        if (strcmp(symbolTable[i].label, label) == 0) {return symbolTable[i].address;} /*found*/
     }
-    return -1;
+    return -1; /*not found*/
 }
 
 FILE* infile =  NULL;
@@ -104,33 +99,217 @@ int main(int argc, char* argv[]) {
         exit(4);
     }
 
-    /* 
-    *
-    *
-    *Do stuff with files 
-    *
-    *
-    */
-
     char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1, *lArg2, *lArg3, *lArg4;
 
     int lRet;
 
-    FILE * lInfile;
-
-    lInfile = fopen( "data.in", "r" );    /* open the input file */
-
+    /*making symbol table*/
     do{
-        lRet = readAndParse( lInfile, lLine, &lLabel, &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
+        int temp;
+        lRet = readAndParse( infile, lLine, &lLabel, &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
         if( lRet != DONE && lRet != EMPTY_LINE )
         {
-            /*...*/
+            
+            /*Populate Symbol Table*/
+            if(strcmp(lOpcode,".orig")==0){baseline=toNum(lArg1);}
+            else if(strcmp(lLabel,"")!=0){
+                printf("line %s\n", lLine);
+                addSymbolTable(lLabel,temp+baseline);
+                
+            }
+
+        }
+        temp+=2;
+    } while( lRet != DONE );
+
+    /*debug symbol table*/
+    int temp;
+    for(temp=0;temp<sym_table_size;temp++){
+        printf("%s, %.04x\n",symbolTable[temp].label,symbolTable[temp].address);
+    }
+
+    rewind(infile);
+
+    do{
+        lRet = readAndParse( infile, lLine, &lLabel, &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
+        char* op = lOpcode;
+        if( lRet != DONE && lRet != EMPTY_LINE )
+        {
+            int ins,dr,sr1,sr2; /*ins(truction)*/
+            /*Parse instructions*/
+            /*1. Parse Opcode and create instruction*/
+            /*2. For each opcode, check the formatting of arguments*/
+            /*3.Throughout, append values to instruction*/
+            /*4. print to file*/
+
+            if (strcmp(op,"add")==0){
+                ins=0x1000;
+                dr=regParse(lArg1);
+                sr1 = regParse(lArg2);
+                if(regParse(lArg2)==-1){sr2 = toNum(lArg2)+0x20;}
+                else{sr2 = regParse(lArg2);}
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }         
+            else if (strcmp(op,"and")==0){
+                ins=0x5000;
+                dr=regParse(lArg1);
+                sr1 = regParse(lArg2);
+                if(regParse(lArg2)==-1){sr2 = toNum(lArg2)+0x20;}
+                else{sr2 = regParse(lArg2);}
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            /*various branches*/
+            else if(strcmp(op,"nop")==0)   {
+                ins=0x0000;
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brp")==0)  {
+                ins=0x0000+(0x1<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brz")==0)  {
+                ins=0x0000+(0x2<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brzp")==0) {
+                ins=0x0000+(0x3<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brn")==0)  {
+                ins=0x0000+(0x4<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brnp")==0) {
+                ins=0x0000+(0x5<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brnz")==0) {
+                ins=0x0000+(0x6<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"br")==0)   {
+                ins=0x0000+(0x7<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            }
+            else if (strcmp(op,"brnzp")==0){
+                ins=0x0000+(0x7<<9);
+                dr=searchSymbolTable(lArg1);
+                if(dr==-1){printf("undefined label %s\n",dr);/*exit(1);*/}
+                else{ins+=((dr-baseline)<<1);}
+            } 
+
+            else if(strcmp(op,"jmp")==0)  {ins=0xC000+(regParse(lArg1)<<6);} 
+            else if(strcmp(op,"ret")==0)  {ins=0xC000+(regParse(lArg1)<<6);}
+            else if(strcmp(op,"jsr")==0)  {ins=0x4000+0x800+toNum(lArg1);}
+            else if(strcmp(op,"jsrr")==0) {ins=0x4000+toNum(lArg1);}
+
+            else if(strcmp(op,"ldb")==0)  {
+                ins=0x2000;
+                dr=regParse(lArg1);
+                sr1=regParse(lArg2);
+                sr2=toNum(lArg3);
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            else if(strcmp(op,"ldw")==0)  {
+                ins=0x6000;
+                dr=regParse(lArg1);
+                sr1=regParse(lArg2);
+                sr2=toNum(lArg3);
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            else if(strcmp(op,"lea")==0)  {
+                /*search sym table, and use that addr - baseline = offset*/
+                ins=0xE000;
+                dr=regParse(lArg1);
+                sr1=searchSymbolTable(lArg2);
+                ins+=((dr<<9)+((sr1-baseline)<<1));
+                /*if(sr1==-1){printf("undefined label %s\n",sr1);exit(1);}
+                else{
+                    ins+=((dr<<9)+(sr1<<1);
+                }*/
+            }
+            else if(strcmp(op,"rti")==0)  {ins=0x8000;}
+            else if(strcmp(op,"lshf")==0) {*cond=0; ins=0xD000;} /*left shift*/
+            else if(strcmp(op,"rshfl")==0){*cond=1; ins=0xD000;} /*right logical shift*/
+            else if(strcmp(op,"rshfa")==0){*cond=3; ins=0xD000;} /*right arithmetic shift*/
+            else if(strcmp(op,"stb")==0)  {
+                ins=0x3000;
+                dr=regParse(lArg1);
+                sr1=regParse(lArg2);
+                sr2=toNum(lArg3);
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            else if(strcmp(op,"stw")==0)  {
+                ins=0x7000;
+                dr=regParse(lArg1);
+                sr1=regParse(lArg2);
+                sr2=toNum(lArg3);
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            else if(strcmp(op,"trap")==0) {ins=0xF000+toNum(lArg1);} /*various trap vects*/
+            else if(strcmp(op,"halt")==0) {ins=0xF000+toNum(lArg1);} /*aka x25*/
+            else if(strcmp(op,"xor")==0)  {
+                ins=0x9000;
+                dr=regParse(lArg1);
+                sr1 = regParse(lArg2);
+                if(regParse(lArg2)==-1){sr2 = toNum(lArg2)+0x20;}
+                else{sr2 = regParse(lArg2);}
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            } 
+            else if(strcmp(op,"not")==0)  {
+                ins=0x9000;
+                dr=regParse(lArg1);
+                sr1 = regParse(lArg2);
+                if(regParse(lArg2)==-1){sr2 = toNum(lArg2)+0x20;}
+                else{sr2 = regParse(lArg2);}
+                ins=ins+(dr<<9)+(sr1<<6)+sr2;
+            }
+            /*BELOW ARE PSEUDO-OPS*/
+            else if(strcmp(op,".orig")==0) {ins=toNum(lArg1);}
+            else if(strcmp(op,".fill")==0) {ins=toNum(lArg1);}
+            else if(strcmp(op,".end")==0)  {;}
+            else {
+                printf("invalid opcode %s\n",op);
+                exit(2);
+            }
+
+            fprintf( outfile, "0x%.4X\n",ins);    /* where lInstr is declared as an int */
+
+
         }
     } while( lRet != DONE );
 
-
     fclose(infile);
     fclose(outfile);
+    free(cond);
+}
+
+int regParse(char* r){
+    if(strcmp(r,"r0")==0){return 0x0;}
+    else if(strcmp(r,"r1")==0){return 0x1;}
+    else if(strcmp(r,"r2")==0){return 0x2;}
+    else if(strcmp(r,"r3")==0){return 0x3;}
+    else if(strcmp(r,"r4")==0){return 0x4;}
+    else if(strcmp(r,"r5")==0){return 0x5;}
+    else if(strcmp(r,"r6")==0){return 0x6;}
+    else if(strcmp(r,"r7")==0){return 0x7;}
+    else {return -1;}
 }
 
 
@@ -151,7 +330,7 @@ int toNum( char * pStr ){
 
         t_ptr = pStr;
         t_length = strlen(t_ptr);
-        for(k=0;k < t_length;k++){
+        for(k=0;k < t_length-1;k++){
             if (!isdigit(*t_ptr)){
                 printf("Error: invalid decimal operand, %s\n",orig_pStr);
                 exit(4);
@@ -241,7 +420,7 @@ int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
 /* Note: MAX_LINE_LENGTH, OK, EMPTY_LINE, and DONE are defined values */
 
 
-/*Returns 0 if not an opcode; 1 otherwise */
+/*Returns -1 if not an opcode; 1 otherwise */
 int isOpcode(char * code){
     int x;
     for(x=0;x<numOpCodes;x++){
@@ -258,40 +437,3 @@ int isOpcode(char * code){
 * Condition codes will also be repurposed for other instructions with shared opcodes.
 * These are in the order presented on pg 6 of LC-3b_ISA.pdf for future reference.
 */
-int parseOpcodes(char* op, int* nzp){
-    *cond=0;
-
-    if     (strcmp(op,"add")==0)  {return 1;}
-    else if(strcmp(op,"and")==0)  {return 5;}
-    else if(strcmp(op,"nop")==0)  {*cond=0; return 0;} /*br variants*/
-    else if(strcmp(op,"brp")==0)  {*cond=1; return 0;}
-    else if(strcmp(op,"brz")==0)  {*cond=2; return 0;}
-    else if(strcmp(op,"brzp")==0) {*cond=3; return 0;}
-    else if(strcmp(op,"brn")==0)  {*cond=4; return 0;}
-    else if(strcmp(op,"brnp")==0) {*cond=5; return 0;}
-    else if(strcmp(op,"brnz")==0) {*cond=6; return 0;}
-    else if(strcmp(op,"br")==0)   {*cond=7; return 0;}
-    else if(strcmp(op,"brnzp")==0){*cond=7; return 0;}
-    else if(strcmp(op,"jmp")==0)  {*cond=0; return 12;} /*cond. code is failsafe; though jmp & ret have same opcodes,*/
-    else if(strcmp(op,"ret")==0)  {*cond=1; return 12;} /*they have no separate cond. codes; ret = jmp from r7*/
-    else if(strcmp(op,"jsr")==0)  {*cond=1; return 4;}
-    else if(strcmp(op,"jsrr")==0) {*cond=0; return 4;}
-    else if(strcmp(op,"ldb")==0)  {return 2;}
-    else if(strcmp(op,"ldw")==0)  {return 6;}
-    else if(strcmp(op,"lea")==0)  {return 14;}
-    else if(strcmp(op,"rti")==0)  {return 8;}
-    else if(strcmp(op,"lshf")==0) {*cond=0; return 13;} /*left shift*/
-    else if(strcmp(op,"rshfl")==0){*cond=1; return 13;} /*right logical shift*/
-    else if(strcmp(op,"rshfa")==0){*cond=3; return 13;} /*right arithmetic shift*/
-    else if(strcmp(op,"stb")==0)  {return 3;}
-    else if(strcmp(op,"stw")==0)  {return 7;}
-    else if(strcmp(op,"trap")==0) {*cond=0; return 15;} /*various trap vects*/
-    else if(strcmp(op,"halt")==0) {*cond=37;return 15;} /*aka x25*/
-    else if(strcmp(op,"xor")==0)  {return 9;} /*!!!there is an xor with cond. code 1, of which not is a special case,*/
-    else if(strcmp(op,"not")==0)  {*cond=1;return 9;} /*but there is no easy way to identify them with this function alone*/
-    /*BELOW ARE PSEUDO-OPS*/
-    else if(strcmp(op,".orig")==0) {return 16;}
-    else if(strcmp(op,".fill")==0) {return 17;}
-    else if(strcmp(op,".end")==0)  {return 18;}
-    else {return -1;} /*invalid opcode*/
-}
--- Lab1/shuffle.asm
+++ Lab1/shuffle.asm
@@ -1,13 +1,9 @@
 .ORIG x3000
-
-LD R0, x4000 ;1st byte
-LD R1, x4001 ;2nd byte
-LD R2, x4002 ;3rd byte
-LD R3, x4003 ;4th byte
-
-LD R4, x4004 ;shuffle mask
-
+LDB R0, x4000 ;1st byte
+LDB R1, x4001 ;2nd byte
+LDB R2, x4002 ;3rd byte
+LDB R3, x4003 ;4th byte
+LDB R4, x4004 ;shuffle mask
 
 HALT
-
 .END
